158c1fe947b0068af4aa8991e1e71f06601fd83c
Andrew Nascimento <81823870+AndrewPolengolas@users.noreply.github.com>
2021-06-18 15:52:23 -0300

    Analise de caso do codigo

---
+++ AulasMJV/src/myapp/cadastros/Empresa.java
@@ -0,0 +1,5 @@
+package myapp.cadastros;
+
+public class Empresa {
+
+}
--- AulasMJV/src/myapp/pedidos/Pedido.java
+++ AulasMJV/src/myapp/pedidos/Pedido.java
@@ -10,9 +10,28 @@ public class Pedido {
 	private Integer id;
 	private Date data;
 	private Double valorTotal;
+	private Integer ccf;
+	private Integer coo;
 	private Cadastro comprador;
 	private List<PedidoItem> itens;
 	
+	
+	public Integer getCcf() {
+		return ccf;
+	}
+
+	public void setCcf(Integer ccf) {
+		this.ccf = ccf;
+	}
+
+	public Integer getCoo() {
+		return coo;
+	}
+
+	public void setCoo(Integer coo) {
+		this.coo = coo;
+	}
+
 	public Cadastro getComprador() {
 		return comprador;
 	}
